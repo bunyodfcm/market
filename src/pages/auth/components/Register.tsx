fe4e7112a0287c7cd675f6becd4eb680859575ff
Muhammad-Devel <jumaboyevmuhammad969@gmail.com>
2025-09-07 22:42:22 +0500

    edit

--- src/pages/auth/components/Register.tsx
+++ src/pages/auth/components/Register.tsx
@@ -60,7 +60,7 @@ const Login: React.FC = () => {
             value={phone}
             onChange={(e) => setPhone(e.target.value)}
             className="mt-1 appearance-none relative block w-full pl-9 pr-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm placeholder:text-gray-400"
-            placeholder="Nickname"
+            placeholder="Phone Number"
           />
           <div className="absolute inset-y-0 left-2 flex items-center pointer-events-none">
             <Icon
