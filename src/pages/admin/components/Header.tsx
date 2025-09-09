092eb2940ba844fb6934946f40b9699df95a2f87
Muhammad-Devel <jumaboyevmuhammad969@gmail.com>
2025-09-10 00:01:53 +0500

    Refactor Header component for improved styling and functionality

--- src/pages/admin/components/Header.tsx
+++ src/pages/admin/components/Header.tsx
@@ -3,7 +3,7 @@ import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Icon } from "@iconify/react/dist/iconify.js";
 
 const Header: React.FC = () => {
-  const user = JSON.parse(localStorage.getItem("user") || "{}");
+
   const [searchInput, setSearchInput] = useState("");
   const [darkmode, setDarkmode] = useState(false);
 
