32313b0ec97d7d2738b6c2f641357b4b62aaee05
Muhammad-Devel <jumaboyevmuhammad969@gmail.com>
2025-10-02 23:42:25 +0500

    add products page

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -6,6 +6,7 @@ const LoginPage = lazy(() => import('../../pages/login'));
 const DashboardPage = lazy(() => import('../../pages/dashboard'));
 const UsersPage = lazy(() => import('../../pages/users'));
 const SettingsPage = lazy(() => import('../../pages/settings'));
+const ProductsPage = lazy(() => import('../../pages/products'));
 
 // Guards
 import { AuthGuard, GuestGuard } from './guards';
@@ -46,6 +47,14 @@ export const AppRoutes = () => {
           element={
             <AuthGuard>
               <SettingsPage />
+            </AuthGuard>
+          } 
+        />
+        <Route 
+          path="/products" 
+          element={
+            <AuthGuard>
+              <ProductsPage />
             </AuthGuard>
           } 
         />
