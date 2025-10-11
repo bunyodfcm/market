95857883469ffe09355f4e4ab83f29e9067a1345
Muhammad-Devel <jumaboyevmuhammad969@gmail.com>
2025-10-11 22:57:32 +0500

    edit

--- src/pages/products/AddProduct.tsx
+++ src/pages/products/AddProduct.tsx
@@ -13,7 +13,7 @@ const CreateProduct: React.FC = () => {
             {t.products.createProduct}
           </h1>
           <button
-            className="inline-flex items-center px-3 py-2 border border-2 border-gray-200 text-sm font-medium rounded-md text-gray-500 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            className="inline-flex items-center px-3 py-2 border-2 border-gray-200 text-sm font-medium rounded-md text-gray-500 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
             onClick={() => {
               navigate(-1);
             }}
